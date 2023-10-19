8b86ef79602b815587bfd10791bf2802ebd185a0
Akbar Albarokah <akbaralbarokah0@gmail.com.com>
2023-10-19 14:15:42 +0700

    benerin sidebar

--- components/sidebar.inc.php
+++ components/sidebar.inc.php
@@ -1,62 +1,77 @@
 <nav id="sidebar" class="sidebar js-sidebar">
   <div class="sidebar-content js-simplebar">
     <a class="sidebar-brand" href="index.html">
-      <img src="images/avatars/avatar.jpg" class="avatar img-fluid rounded-5 me-1" alt="Charles Hall" />
+    <img src="images/avatars/avatar.jpg" class="avatar img-fluid rounded-5 me-1" alt="Charles Hall" />
       <span class="align-middle">OLAMS</span>
     </a>
 
     <ul class="sidebar-nav">
+      <li class="sidebar-header">
+        Menu
+      </li>
 
-      <!-- <li class="sidebar-item">
+      <li class="sidebar-item active">
         <a class="sidebar-link" href="index.html">
-          <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
-        </a>
-      </li> -->
-
-      <!-- <li class="sidebar-item">
-        <a data-bs-target="#dashboards" data-bs-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">
-          <i class="align-middle" data-feather="clock"></i><span class="align-middle">Overtime </span>
-        </a>
-        <ul id="dashboards" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-          <li class="sidebar-item"><a class="sidebar-link" href="/dashboard-ecommerce">Overtime Normal</a></li>
-          <li class="sidebar-item"><a class="sidebar-link" href="/dashboard-crypto">Duty Overtime</a></li>
-        </ul>
-      </li> -->
-
-      <!-- <li class="sidebar-item">
-        <a class="sidebar-link" href="pages-sign-in.html">
-          <i class="align-middle" data-feather="clipboard"></i> <span class="align-middle">Leave</span>
-        </a>
-      </li> -->
-
-      <!-- <li class="sidebar-item">
-        <a class="sidebar-link" href="pages-sign-up.html">
-          <i class="align-middle" data-feather="calendar"></i> <span class="align-middle">Attendances</span>
-        </a>
-      </li> -->
-
-      <li class="sidebar-item">
-        <a class="sidebar-link" href="userlist.php">
-          <i class="align-middle" data-feather="user"></i> <span class="align-middle">User List</span>
+          <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Dashboard</span>
         </a>
       </li>
 
-      <!-- <li class="sidebar-item">
-        <a data-bs-target="#dashboards" data-bs-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">
-          <i class="align-middle" data-feather="file-text"></i><span class="align-middle">Master Data</span>
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="pages-profile.html">
+          <i class="align-middle" data-feather="clock"></i> <span class="align-middle">Overtime</span>
         </a>
-        <ul id="dashboards" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-          <li class="sidebar-item"><a class="sidebar-link" href="/dashboard-ecommerce">Mater Project</a></li>
-          <li class="sidebar-item"><a class="sidebar-link" href="/dashboard-crypto">Master Divisi</a></li>
-          <li class="sidebar-item"><a class="sidebar-link" href="/dashboard-crypto">Master Basic Salary</a></li>
-        </ul>
-      </li> -->
+      </li>
 
-      <!-- <li class="sidebar-item">
-        <a class="sidebar-link" href="ui-forms.">
-          <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Report</span>
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="pages-sign-in.html">
+          <i class="align-middle" data-feather="calendar"></i> <span class="align-middle">Leave</span>
         </a>
-      </li> -->
+      </li>
+
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="pages-sign-up.html">
+          <i class="align-middle" data-feather="file-text"></i> <span class="align-middle">Attendance</span>
+        </a>
+      </li>
+
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="pages-blank.html">
+          <i class="align-middle" data-feather="clipboard"></i> <span class="align-middle">Report</span>
+        </a>
+      </li>
+
+      <li class="sidebar-header">
+        Master Data
+      </li>
+
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="ui-buttons.html">
+          <i class="align-middle" data-feather="square"></i> <span class="align-middle">Master Project</span>
+        </a>
+      </li>
+
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="ui-forms.html">
+          <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Master Division</span>
+        </a>
+      </li>
+
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="ui-cards.html">
+          <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Master Basic Salary</span>
+        </a>
+      </li>
+
+      <li class="sidebar-header">
+        User Management
+      </li>
+
+      <li class="sidebar-item">
+        <a class="sidebar-link" href="userlist.php">
+          <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">User List</span>
+        </a>
+      </li>
     </ul>
+
   </div>
 </nav>
