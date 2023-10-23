5a5f65fc9c994c3164f02a69a51f3f422abe93d8
fajrialfauzi <fajrial39@gmail.com>
2023-10-23 11:23:36 +0700

    fix halaman login part 1

--- script.inc.php
+++ script.inc.php
@@ -1,2 +1,4 @@
+<script src="javascripts/bootstrap.min.js"></script>
+<script src="javascripts/popper.min.js"></script>
 <script src="javascripts/app.min.js"></script>
 <script src="javascripts/custom.js"></script>
