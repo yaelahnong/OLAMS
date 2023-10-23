c3bbd0494fbdd62d941b394f6b893f80c57c1daa
Akbar Albarokah <akbaralbarokah0@gmail.com.com>
2023-10-23 09:47:37 +0700

    in progress: ui dashboard

---
+++ dashboard.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include "head.inc.php"; ?>
+  <title>OLAMS - User List</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <?php include "components/sidebar.inc.php"; ?>
+    <div class="main">
+      <?php include "components/navbar.inc.php"; ?>
+      <main class="content">
+        <div class="container-fluid p-0">
+          <h1 class="h1 mb-3 judul_halaman"><strong>Dashboard</strong></h1>
+          <div class="row d-flex flex-row">
+            <div class="col-sm-3">
+              <div class="card ml-auto">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col mt-0">
+                      <h5 class="card-title">Sales</h5>
+                    </div>
+
+                    <div class="col-auto">
+                      <div class="stat text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck align-middle">
+                          <rect x="1" y="3" width="15" height="13"></rect>
+                          <polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon>
+                          <circle cx="5.5" cy="18.5" r="2.5"></circle>
+                          <circle cx="18.5" cy="18.5" r="2.5"></circle>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <h1 class="mt-1 mb-3">2.382</h1>
+                  <div class="mb-0">
+                    <span class="badge badge-primary-light"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
+                    <span class="text-muted">Since last week</span>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col mt-0">
+                      <h5 class="card-title">Sales</h5>
+                    </div>
+
+                    <div class="col-auto">
+                      <div class="stat text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck align-middle">
+                          <rect x="1" y="3" width="15" height="13"></rect>
+                          <polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon>
+                          <circle cx="5.5" cy="18.5" r="2.5"></circle>
+                          <circle cx="18.5" cy="18.5" r="2.5"></circle>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <h1 class="mt-1 mb-3">2.382</h1>
+                  <div class="mb-0">
+                    <span class="badge badge-primary-light"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
+                    <span class="text-muted">Since last week</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="card ml-auto">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col mt-0">
+                      <h5 class="card-title">Sales</h5>
+                    </div>
+
+                    <div class="col-auto">
+                      <div class="stat text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck align-middle">
+                          <rect x="1" y="3" width="15" height="13"></rect>
+                          <polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon>
+                          <circle cx="5.5" cy="18.5" r="2.5"></circle>
+                          <circle cx="18.5" cy="18.5" r="2.5"></circle>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <h1 class="mt-1 mb-3">2.382</h1>
+                  <div class="mb-0">
+                    <span class="badge badge-primary-light"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
+                    <span class="text-muted">Since last week</span>
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col mt-0">
+                      <h5 class="card-title">Sales</h5>
+                    </div>
+
+                    <div class="col-auto">
+                      <div class="stat text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck align-middle">
+                          <rect x="1" y="3" width="15" height="13"></rect>
+                          <polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon>
+                          <circle cx="5.5" cy="18.5" r="2.5"></circle>
+                          <circle cx="18.5" cy="18.5" r="2.5"></circle>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <h1 class="mt-1 mb-3">2.382</h1>
+                  <div class="mb-0">
+                    <span class="badge badge-primary-light"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
+                    <span class="text-muted">Since last week</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <?php include "components/footer.inc.php"; ?>
+    </div>
+  </div>
+  <?php include "script.inc.php"; ?>
+</body>
+
+</html>
